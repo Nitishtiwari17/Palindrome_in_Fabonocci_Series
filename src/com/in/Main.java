0f478e9230c986960ad29abae0bd806dd27f0480
Nitish Kumar Tiwari <94640716+Nitishtiwari17@users.noreply.github.com>
2023-07-21 23:13:22 +0530

    Initial commit

---
+++ src/com/in/Main.java
@@ -0,0 +1,19 @@
+package com.in;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=0;i<n;i++){
+            int a = 0,b=1;
+            for(int j=0;j<=i;j++){
+                System.out.print(a+" ");
+                int c  = a+b;
+                a = b;
+                b = c;
+            }
+            System.out.println();
+
+        }
+    }
+}
